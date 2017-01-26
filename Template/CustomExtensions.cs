5e5abfe5bfd5ef31a78b0a95a9693c4bbad6821f
Sicryption <Sicryption@gmail.com>
2017-01-26 17:28:19 -0500

    Template Update

---
+++ Template/CustomExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using SharpDX;
+
+namespace Template
+{
+    static class CustomExtensions
+    {
+        public static bool MeetsCriteria(this Obj_AI_Base target)
+        {
+            if (!target.IsDead && target.IsVisible && !target.IsInvulnerable && target.IsTargetable && target.IsHPBarRendered)
+                return true;
+            return false;
+        }
+        public static bool GetCheckboxValue(this Menu self, string text)
+        {
+            return MenuHandler.GetCheckboxValue(self, text);
+        }
+        public static int GetSliderValue(this Menu self, string text)
+        {
+            return MenuHandler.GetSliderValue(self, text);
+        }
+        public static string GetComboBoxText(this Menu self, string text)
+        {
+            return MenuHandler.GetComboBoxText(self, text);
+        }
+        public static List<Obj_AI_Base> ToObj_AI_BaseList(this List<AIHeroClient> list)
+        {
+            List<Obj_AI_Base> returnList = new List<Obj_AI_Base>();
+            foreach (AIHeroClient unit in list.Where(a => a.MeetsCriteria()))
+                returnList.Add(unit as Obj_AI_Base);
+            return returnList;
+        }
+        public static List<Obj_AI_Base> ToObj_AI_BaseList(this List<Obj_AI_Minion> list)
+        {
+            List<Obj_AI_Base> returnList = new List<Obj_AI_Base>();
+            foreach (Obj_AI_Minion unit in list.Where(a => a.MeetsCriteria()))
+                returnList.Add(unit as Obj_AI_Base);
+            return returnList;
+        }
+        public static float MissingHealth(this AIHeroClient self)
+        {
+            return self.MaxHealth - self.Health;
+        }
+        public static float BonusAttackDamage(this AIHeroClient self)
+        {
+            return self.TotalAttackDamage - self.BaseAttackDamage;
+        }
+        public static InventorySlot GetItem(this AIHeroClient self, ItemId item)
+        {
+            return self.InventoryItems.Where(a => a.Id == item).FirstOrDefault();
+        }
+        public static bool CanCancleCC(this AIHeroClient self)
+        {
+            return (self.HasBuffOfType(BuffType.Blind)
+                || self.HasBuffOfType(BuffType.Charm)
+                || self.HasBuffOfType(BuffType.Fear)
+                || self.HasBuffOfType(BuffType.Knockback)
+                || self.HasBuffOfType(BuffType.Silence)
+                || self.HasBuffOfType(BuffType.Snare)
+                || self.HasBuffOfType(BuffType.Stun)
+                || self.HasBuffOfType(BuffType.Taunt))
+                //not being knocked back by dragon
+                && !self.HasBuff("moveawaycollision")
+                //not standing on raka silence
+                && !self.HasBuff("sorakaepacify")
+                && !self.HasBuff("plantsatchelknockback");
+        }
+        public static bool MeetsCriteria(this InventorySlot item)
+        {
+            if (item != null && item.CanUseItem() && !ModeHandler.hasDoneActionThisTick)
+                return true;
+            return false;
+        }
+        public static bool IsAutoCanceling(this AIHeroClient self, List<Obj_AI_Base> enemies)
+        {
+            return Orbwalker.IsAutoAttacking || enemies.Where(a => a.IsInRange(self, self.GetAutoAttackRange())).FirstOrDefault() == null;
+        }
+        public static bool ContainsAny(this string s, bool CaseSensitive, params string[] text)
+        {
+            List<string> temp = text.ToList();
+            if (!CaseSensitive)
+            {
+                List<string> NonCaseSensitiveList = new List<string>();
+                foreach (string str in temp)
+                    NonCaseSensitiveList.Add(str.ToLower());
+                temp = NonCaseSensitiveList;
+            }
+
+            foreach (string str in temp)
+                if (s.ToLower().Contains(str.ToLower()))
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ Template/ModeHandler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using SharpDX;
+
+namespace Template
+{
+    class ModeHandler
+    {
+        public static AIHeroClient Champion => Player.Instance;
+        public static bool hasDoneActionThisTick = false;
+
+        public static void Combo()
+        {
+            Menu menu = MenuHandler.Combo;
+            List<Obj_AI_Base> enemies = EntityManager.Heroes.Enemies.ToList().ToObj_AI_BaseList();
+
+        }
+
+        public static void Harass()
+        {
+            Menu menu = MenuHandler.Harass;
+            List<Obj_AI_Base> enemies = EntityManager.Heroes.Enemies.ToList().ToObj_AI_BaseList();
+
+        }
+
+        public static void AutoHarass()
+        {
+            if (Champion.IsUnderEnemyturret())
+                return;
+
+            Menu menu = MenuHandler.AutoHarass;
+            List<Obj_AI_Base> enemies = EntityManager.Heroes.Enemies.ToList().ToObj_AI_BaseList();
+
+        }
+
+        public static void JungleClear()
+        {
+            Menu menu = MenuHandler.JungleClear;
+            List<Obj_AI_Base> enemies = EntityManager.MinionsAndMonsters.Monsters.ToList().ToObj_AI_BaseList();
+
+        }
+
+        public static void Killsteal()
+        {
+            Menu menu = MenuHandler.Killsteal;
+            List<Obj_AI_Base> enemies = EntityManager.Heroes.Enemies.ToList().ToObj_AI_BaseList();
+            
+
+        }
+        
+        public static void Flee()
+        {
+            Menu menu = MenuHandler.Flee;
+            List<Obj_AI_Base> enemies = EntityManager.Heroes.Enemies.ToList().ToObj_AI_BaseList();
+
+        }
+
+        public static void LaneClear()
+        {
+            Menu menu = MenuHandler.LaneClear;
+            List<Obj_AI_Base> enemies = EntityManager.MinionsAndMonsters.EnemyMinions.ToList().ToObj_AI_BaseList();
+
+        }
+
+        public static void LastHit()
+        {
+            Menu menu = MenuHandler.LastHit;
+            List<Obj_AI_Base> enemies = EntityManager.MinionsAndMonsters.EnemyMinions.ToList().ToObj_AI_BaseList();
+
+        }
+        
+        public static void CastQ(List<Obj_AI_Base> enemies, bool ks, Menu menu)
+        {
+
+        }
+
+        public static void CastW(List<Obj_AI_Base> enemies, bool ks, Menu menu)
+        {
+
+        }
+
+        public static void CastE(List<Obj_AI_Base> enemies, bool ks, Menu menu)
+        {
+
+        }
+
+        public static void CastR(List<Obj_AI_Base> enemies, bool ks, Menu menu)
+        {
+
+        }
+
+
+        public static void UseItems()
+        {
+            Menu menu = MenuHandler.Items;
+
+            #region Item Initialization
+            //InventorySlot SeraphsEmbrace = menu.GetCheckboxValue("Seraphs Embrace") ? Ryze.GetItem(ItemId.Seraphs_Embrace) : null,
+            //    Zhonyas = menu.GetCheckboxValue("Zhonyas Hourglass") ? Ryze.GetItem(ItemId.Zhonyas_Hourglass) : null;
+            #endregion
+
+        }
+    }
+}
